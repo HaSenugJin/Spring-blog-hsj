69cc538425472c31b92ab7dc0b0604ec3d1d1bc3
hyeon <lhyeonnit@gmail.com>
2024-03-21 15:30:46 +0900

    글쓰기 완료

--- src/main/java/shop/mtcoding/blog/controller/board/BoardController.java
+++ src/main/java/shop/mtcoding/blog/controller/board/BoardController.java
@@ -1,11 +1,16 @@
-package shop.mtcoding.blog.controller;
+package shop.mtcoding.blog.controller.board;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
+@RequiredArgsConstructor
 @Controller
 public class BoardController {
+
+    private final BoardNativeRepository boardNativeRepository;
 
     @GetMapping("/" )
     public String index() {
@@ -20,5 +25,12 @@ public class BoardController {
     @GetMapping("/board/{id}")
     public String detail(@PathVariable Integer id) {
         return "board/detail";
+    }
+
+    @PostMapping("/board/save")
+    public String save(String username, String title, String content) {
+        boardNativeRepository.save(username, title, content);
+
+        return "redirect:/";
     }
 }
