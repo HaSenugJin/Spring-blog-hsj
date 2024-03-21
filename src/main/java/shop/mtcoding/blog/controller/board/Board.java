f783147d8d1d1ad92bca55aa47fb431c168ac4eb
hyeon <lhyeonnit@gmail.com>
2024-03-21 16:27:06 +0900

    게시글 작성시간

--- src/main/java/shop/mtcoding/blog/controller/board/Board.java
+++ src/main/java/shop/mtcoding/blog/controller/board/Board.java
@@ -2,6 +2,7 @@ package shop.mtcoding.blog.controller.board;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import shop.mtcoding.blog.controller.util.MyDateUtil;
 
 import java.sql.Timestamp;
 
@@ -17,4 +18,8 @@ public class Board {
     private String content;
     private String username;
     private Timestamp createdAt;
+
+    public String getTime() {
+        return MyDateUtil.timestampFormat(createdAt);
+    }
 }
