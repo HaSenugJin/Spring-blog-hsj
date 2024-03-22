288d255d09b60e2c14741f7c9d80b44ca650af46
hyeon <lhyeonnit@gmail.com>
2024-03-22 16:26:03 +0900

    에러메시지 인터셉터까지

---
+++ src/main/java/shop/mtcoding/blog/_core/config/WebMvcConfig.java
@@ -0,0 +1,22 @@
+package shop.mtcoding.blog._core.config;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import shop.mtcoding.blog._core.errors.exception.Exception401;
+import shop.mtcoding.blog._core.interceptor.LoginInterceptor;
+import shop.mtcoding.blog.controller.user.User;
+
+@Configuration // IoC
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginInterceptor())
+                .addPathPatterns("/board/**", "/user/**")
+                .excludePathPatterns("/board/{id:\\d+}");
+    }
+}
